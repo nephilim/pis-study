1d96e2de08d6ef96f37d14938f7ec86d1f79ad1b
nephilim <i015005@gmail.com>
2012-05-29 01:12:07 +0900

    테스트 케이스 정비 및 테스트용 이미지 폴더(images) 추가

--- src/test/scala/pis/chap23/practice/ExitExtractGeneratorSpec.scala
+++ src/test/scala/pis/chap23/practice/ExitExtractGeneratorSpec.scala
@@ -5,23 +5,24 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ExifExtractorGeneratorSpec extends Spec {
-  val DIR_NAME = "/Users/skplanet/images"
+  val DIR_NAME = "images"
 
   describe("Metadata 추출") {
     it("상속 관계 확인") {
       val extractor = new ExifGifExtractor(DIR_NAME)
       assert( extractor.metadatas.size == 3)  	// image 파일 3개 
     }
-    it("GPS 정보 추출") {
+    it("GPS 정보(위도, 경도) 추출") {
       val extractor = new ExifGifExtractor(DIR_NAME)
       val metadata = extractor.metadatas(0)
-      assert(extractor.extractGpsPos(metadata).toString 
-          == "(37.0° 32.0\' 5.399999999993383\",126.0° 59.0\' 39.59999999998786\")")
+      assert(
+          extractor.extractGpsPos(metadata).toString ==  
+          "(37.0° 32.0\' 5.399999999993383\",126.0° 59.0\' 39.59999999998786\")")
     }
     it("Generator 동작확인") {
       val extractor = new ExifGifExtractor(DIR_NAME)
-      for( position <- extractor ) {	// position: Tuple[String,String]
-        Console.println("위도 %s, 경도 %s".format(position._1, position._2))
+      for( position:(String,String) <- extractor ) {
+        
       }
     }
   }
